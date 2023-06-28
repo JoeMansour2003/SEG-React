1a888ce20e839cbe8ee5ca5da9bcd95b5ccafb62
Joe Mansour <joemansour2003@gmail.com>
2023-06-28 00:27:49 -0400

    Nav bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Index from "./pages/Index";
+import About from "./pages/About";
+import Services from "./pages/Services";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+const App = () => {
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route exact path="/" component={Index} />
+                <Route path="/about" component={About} />
+                <Route path="/services" component={Services} />
+            </Routes>
+        </Router>
+    );
+};
 export default App;
---
+++ src/pages/Index.js
@@ -0,0 +1,27 @@
+const Index = () => {
+    return (
+        <div className="container">
+            <h1>About Us</h1>
+            <p>
+                Welcome to our website! We are a team of professionals dedicated
+                to providing high-quality services to our clients.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                aliquet porttitor diam id aliquam. Quisque ultrices fermentum
+                felis, et convallis velit interdum vitae. Nulla interdum
+                interdum enim ut congue. Sed id varius nisi, nec tincidunt dui.
+                Praesent pharetra mauris urna, sed pellentesque neque rutrum
+                sed. Aliquam nec efficitur quam. Pellentesque habitant morbi
+                tristique senectus et netus et malesuada fames ac turpis
+                egestas. Suspendisse tempor facilisis velit, et dictum tortor
+                elementum a. Proin vitae turpis lectus. Fusce in urna velit.
+                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+                posuere cubilia Curae; Suspendisse malesuada sit amet lectus eu
+                dignissim.
+            </p>
+        </div>
+    );
+};
+
+export default Index;
